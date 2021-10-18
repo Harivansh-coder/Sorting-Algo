2a4ff3b09443071ec8caf6562b9607a864c34d57
Harivansh Thakur <harivanshthakur8@gmail.com>
2021-10-18 21:55:43 +0530

    implemented the algorithm

--- sorting algorithm/Bubblesort.java
+++ sorting algorithm/Bubblesort.java
@@ -1,3 +1,7 @@
+/*
+Bubble sort implementation in java
+*/
+
 public class Bubblesort {
 
     int z = 0;
@@ -18,16 +22,20 @@ public class Bubblesort {
 
         Bubblesort bs = new Bubblesort();
 
-        int[] a = {2,1,4,3,5,7};
+        int[] a = {2,1,4,3,5,7,10,3,5,6,64,6,622,1,5};
 
         bs.bsort(a);
 
         for (int i : a){
             System.out.print(i+"  ");
-        }
-
-        
-        
+        } 
     }
     
 }
+
+/*
+# Output
+
+1  1  2  3  3  4  5  5  5  6  6  7  10  64  622 
+
+*/
